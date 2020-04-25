a20ae4269bdb6cc64e82e71758fe08753cb66d0f
Alcazar37 <mralcazar007@gmail.com>
2020-04-24 19:53:37 -0700

    Done!

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "05f02fa502c6ef2ae3b5d9aec3ac69fa"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCRoxxZhuCHAsc9rUMFKZhEK14eRt69gMY"
